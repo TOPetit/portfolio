c7b93ec6b3bfa3160b75dbf7f8f49a1e7717e361
Theo Petit <theo.p83@gmail.com>
2021-10-22 10:49:48 +0200

    init homepage and clock

---
+++ clock.js
@@ -0,0 +1,16 @@
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+
+    let currentTime = hour + ":"
+        + min;
+
+    document.getElementById("clock")
+        .innerHTML = currentTime;
+}
+showTime();
